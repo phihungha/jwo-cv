a3a76720538ca8b8376d85082884376c062e103a
Ha Phi Hung <haphihung55@gmail.com>
2024-04-15 01:26:15 +0700

    Add debug mode

--- jwo_cv/__main__.py
+++ jwo_cv/__main__.py
@@ -10,8 +10,6 @@ from jwo_cv.item_classifier import ItemClassifier
 
 logger = logging.Logger("jwo-cv")
 
-window = cv2.namedWindow("Debug view")
-
 
 def getVideoSource(source_idx: int) -> cv2.VideoCapture:
     """Get a video source.
@@ -33,7 +31,7 @@ def getVideoSource(source_idx: int) -> cv2.VideoCapture:
 
 
 def processVideo(source: cv2.VideoCapture, item_classifier: ItemClassifier):
-    """Process video from provided video source. Use q key to stop.
+    """Process video from provided video source.
 
     Args:
         source (cv2.VideoCapture): Video source
@@ -46,12 +44,33 @@ def processVideo(source: cv2.VideoCapture, item_classifier: ItemClassifier):
 
         items = item_classifier.predict(image)
 
-        annotator = plotting.Annotator(image)
         for item in items:
             logger.debug(item)
+
+
+def processVideoWithDebug(source: cv2.VideoCapture, item_classifier: ItemClassifier):
+    """Process video from provided video source with debug view.
+    Use q key to stop.
+
+    Args:
+        source (cv2.VideoCapture): Video source
+    """
+
+    cv2.namedWindow("Debug view")
+
+    while True:
+        received, image = source.read()
+        if not received:
+            logger.error("Can't receive frame!")
+
+        items = item_classifier.predict(image)
+
+        annotator = plotting.Annotator(image)
+        for item in items:
+            logger.error(item)
             annotator.box_label(
                 item.box.tensor,
-                item.class_name,
+                f"{item.class_name} ({round(item.confidence, 3):.1%})",
                 info.ITEM_ANNOTATION_BOX_COLOR,
                 info.ITEM_ANNOTATION_TEXT_COLOR,
             )
@@ -61,16 +80,23 @@ def processVideo(source: cv2.VideoCapture, item_classifier: ItemClassifier):
         if cv2.waitKey(1) == ord("q"):
             break
 
+    cv2.destroyWindow("Debug view")
+
 
 def main():
     config = toml.load("jwo_cv/config/config.toml")
+    general_config = config["general"]
+
     logging.config.dictConfig(config["logging"])
     video_sources_config = config["video_sources"]
     video_source = getVideoSource(video_sources_config["source_idx"])
     models_config = config["models"]
     item_classifier = ItemClassifier(models_config["item_classifier_model_path"])
 
-    processVideo(video_source, item_classifier)
+    if general_config["debug_video"]:
+        processVideoWithDebug(video_source, item_classifier)
+    else:
+        processVideo(video_source, item_classifier)
 
     video_source.release()
 
