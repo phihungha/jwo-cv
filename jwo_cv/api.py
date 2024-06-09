23e0840092774aefe1807c415294c47b2ca86550
Ha Phi Hung <haphihung55@gmail.com>
2024-06-09 23:44:26 +0700

    Fix not providing socketio url namespace

--- jwo_cv/api.py
+++ jwo_cv/api.py
@@ -1,4 +1,5 @@
 import logging
+import urllib.parse
 from collections.abc import Iterator
 
 import socketio
@@ -9,28 +10,28 @@ from jwo_cv import vision
 logger = logging.getLogger(__name__)
 
 
-def create_message(event: vision.ShoppingEvent) -> list[dict]:
+def create_message(event: vision.ShoppingEvent) -> dict:
     """Create proper event message from a ShoppingEvent instance.
 
     Args:
         event (vision.ShoppingEvent): Shopping event instance
 
     Returns:
-        list[dict]: Event message
+        dict: Event message
     """
 
     if event.type == ad.ActionType.PICK:
-        message = [
+        item_updates = [
             {"productId": name, "quantity": count}
             for name, count in event.item_counts.items()
         ]
     else:
-        message = [
+        item_updates = [
             {"productId": name, "quantity": -count}
             for name, count in event.item_counts.items()
         ]
 
-    return message
+    return {"items": item_updates}
 
 
 def start_emitting_events(server_url: str, events: Iterator[vision.ShoppingEvent]):
@@ -41,8 +42,11 @@ def start_emitting_events(server_url: str, events: Iterator[vision.ShoppingEvent
         events (Iterator[vision.ShoppingEvent]): Shopping events
     """
 
+    url_components = urllib.parse.urlparse(server_url)
+    host = f"{url_components.scheme}://{url_components.netloc}"
+
     with socketio.SimpleClient() as sio:
-        sio.connect(server_url)
+        sio.connect(host, namespace=url_components.path)
         logger.info(f"Connected to server {server_url} and start emitting events.")
 
         for event in events:
